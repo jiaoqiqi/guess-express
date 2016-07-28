c277e951e87f020d280899772745df2ad5d3255b
jiaoqiqi <1176932165@qq.com>
2016-07-29 00:37:57 +0800

    add server,js

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
-/**
- * Created by jiaoqiqi on 16-7-29.
- */
+var express = require('express');
+const CompareNumber = require('./compareNumber');
+const AnswerGenerate = require('./answerGenerator');
+const bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/answer',function (req,res) {
+    const answer=AnswerGenerate.generate();
+    res.send(answer);
+});
+app.post('/compare', function (req, res) {
+
+    const result = CompareNumber.compareNumber(req.body.answer,req.body.input);
+    res.send(result);
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
