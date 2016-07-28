c8ff504ce95c587343f993dd5334d01ba033d61b
jiaoqiqi <1176932165@qq.com>
2016-07-29 00:37:37 +0800

    add compareNumber

--- compareNumber.js
+++ compareNumber.js
@@ -1,3 +1,24 @@
-/**
- * Created by jiaoqiqi on 16-7-28.
- */
+class CompareNumber {
+    static compareNumber(answer, input) {
+        const answerArray = answer.split('');
+        const inputArray = input.split('');
+
+        let a = 0;
+        let b = 0;
+
+        answerArray.map(answer => {
+            const input = inputArray.find(input => answer === input);
+
+            if (input) {
+                const answerPosition = answerArray.indexOf(answer);
+                const inputPosition = inputArray.indexOf(input);
+
+                answerPosition === inputPosition ? a++ : b++;
+            }
+        });
+
+        return `${a}A${b}B`;
+    }
+}
+
+module.exports = CompareNumber;
