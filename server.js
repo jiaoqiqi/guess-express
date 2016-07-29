e115f475a41e3c6c4b14d4520c76773bff2a11b3
jiaoqiqi <1176932165@qq.com>
2016-07-29 10:21:28 +0800

    correct the express route

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require('express');
+const express = require('express');
 const CompareNumber = require('./compareNumber');
-const AnswerGenerate = require('./answerGenerator');
+const AnswerGenerator = require('./answerGenerator');
 const bodyParser = require('body-parser');
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/answer',function (req,res) {
-    const answer=AnswerGenerate.generate();
+app.get('/answer', function (req, res) {
+    const answer = AnswerGenerator.generate();
     res.send(answer);
 });
-app.post('/compare', function (req, res) {
 
-    const result = CompareNumber.compareNumber(req.body.answer,req.body.input);
+app.post('/compare', function (req, res) {
+    const result = CompareNumber.compareNumber(req.body.answer, req.body.input);
     res.send(result);
 });
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+const server = app.listen(3000, () => {
+    const address = server.address().address;
+    const port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', address, port);
 });
